2493c8fdcc55e8c10b7dd23e4ef81cf4d770d5e3
mikemajesty <mikemajesty2011@gmail.com>
2017-03-14 21:27:50 -0300

    configure server js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 var express = require("express");
 
 var app = express();
+
+app.use('/',  express.static(__dirname + '/'));
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.sendfile('demo/index.html');
+});
+
+app.listen(PORT, function() {
+    console.log('Server Running on '+ PORT);
+});
