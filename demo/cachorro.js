783a9cf09f394845cfd83a076f5769df3fc6cae1
mikemajesty <mikemajesty2011@gmail.com>
2017-03-15 22:53:00 -0300

    Learning vuejs: Composing with Components

--- demo/cachorro.js
+++ demo/cachorro.js
@@ -1,18 +1,15 @@
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
 });
 
-var app6 = new Vue({
-  el: '#app-6',
+var app7 = new Vue({
+  el: '#app-7',
   data: {
-    message: 'Hello Vue!'
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
   }
-});
+})
