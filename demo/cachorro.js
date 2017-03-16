9a7f739f7ed358c36cc7e7526e54484d1d7e5b95
mikemajesty <mikemajesty2011@gmail.com>
2017-03-15 22:22:48 -0300

    Learning vuejs: Conditionals and Loops

--- demo/cachorro.js
+++ demo/cachorro.js
@@ -1,6 +1,17 @@
-  var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date()
-    }
-  });
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+});
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+});
