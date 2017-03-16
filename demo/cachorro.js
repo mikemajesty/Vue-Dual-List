e8ed4e8c0af740a961f3ec1574a3e11505386cbf
mikemajesty <mikemajesty2011@gmail.com>
2017-03-15 22:45:37 -0300

    Learning vuejs: Handling User Input

--- demo/cachorro.js
+++ demo/cachorro.js
@@ -1,17 +1,18 @@
-var app3 = new Vue({
-  el: '#app-3',
+var app5 = new Vue({
+  el: '#app-5',
   data: {
-    seen: true
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 });
 
-var app4 = new Vue({
-  el: '#app-4',
+var app6 = new Vue({
+  el: '#app-6',
   data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
+    message: 'Hello Vue!'
   }
 });
