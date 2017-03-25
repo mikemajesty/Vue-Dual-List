5563dfe9cb0b3d9b5a29808eb5f05a1aec1fc5ad
mikemajesty <mikemajesty2011@gmail.com>
2017-03-25 17:29:19 -0300

    fix css bug

--- demo/cachorro.js
+++ demo/cachorro.js
@@ -1,12 +1,17 @@
 Vue.component('celso-dog', {
   template:  `<div class='excaliburduallist'> 
                 <div class='row'>
-                  <div class='col-lg-12 col-md-12 col-sm-12'>
-                    <select class='form-control'>
-                      <option v-for='category in options.categories' v-bind:dto='category'> 
-                          <b >{{ category.name }}</b> 
-                      </option> 
-                    </select> 
+                  <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                    <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                      <h4>
+                        {{options.label}}
+                      </h4>
+                      <select class='form-control'>
+                        <option v-for='category in options.categories' v-bind:dto='category'> 
+                            <b >{{ category.name }}</b> 
+                        </option> 
+                      </select> 
+                   </div> 
                   </div>   
                 </div>  
               </div>` ,
@@ -17,6 +22,7 @@ var vm = new Vue({
   el: '#cachorro',
   data: {
     options: {
+      label: "Demo title",
       categories: [{name: 'FF7'}, {name: 'FF8'}, {name: 'FF9'}]
     } 
   }
