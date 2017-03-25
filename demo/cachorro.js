82b0fbb3aae297ee66172fae2a8182179cf0c715
mikemajesty <mikemajesty2011@gmail.com>
2017-03-25 17:21:37 -0300

    change data structure

--- demo/cachorro.js
+++ demo/cachorro.js
@@ -1,19 +1,23 @@
 Vue.component('celso-dog', {
   template:  `<div class='excaliburduallist'> 
-                <row class='row'>
-                  <select class='form-control'>
-                    <option v-for='category in categories' v-bind:dto='category'> 
-                        <b >{{ category.name }}</b> 
-                    </option> 
-                  </select>  
+                <div class='row'>
+                  <div class='col-lg-12 col-md-12 col-sm-12'>
+                    <select class='form-control'>
+                      <option v-for='category in options.categories' v-bind:dto='category'> 
+                          <b >{{ category.name }}</b> 
+                      </option> 
+                    </select> 
+                  </div>   
                 </div>  
               </div>` ,
-  props: ['dto', 'categories'],
+  props: ['dto', 'options'],
 });
 
 var vm = new Vue({
   el: '#cachorro',
   data: {
-    categories: [{name: 'FF7'}, {name: 'FF8'}, {name: 'FF9'}]
+    options: {
+      categories: [{name: 'FF7'}, {name: 'FF8'}, {name: 'FF9'}]
+    } 
   }
 });
