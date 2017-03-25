0a13421085661d0ce6cb0ac47de4b2fac02ed23c
mikemajesty <mikemajesty2011@gmail.com>
2017-03-25 17:49:01 -0300

    add final layout

--- demo/cachorro.js
+++ demo/cachorro.js
@@ -6,14 +6,43 @@ Vue.component('celso-dog', {
                       <h4>
                         {{options.label}}
                       </h4>
-                      <select class='form-control'>
+                      <select class='form-control' v-model='search'>
                         <option v-for='category in options.categories' v-bind:dto='category'> 
                             <b >{{ category.name }}</b> 
                         </option> 
                       </select> 
                    </div> 
-                  </div>   
-                </div>  
+                  </div>
+                </div>
+                 <div class="row">
+                  <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="row">
+                      <div class="col-lg-8 col-md-8 col-sm-8 pull-right"> 
+                        <button type="button" class="btn btn-default btn-xs"> 
+                          Move All -&#187; 
+                        </button> 
+                      </div>
+                    </div>
+                    <div class="list">
+                      <ul>
+                      
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-7 col-sm-7"> 
+                          <button type="button" class="btn btn-default btn-xs pull-right"> 
+                            &#171;- Move All 
+                          </button> 
+                        </div>
+                    </div>
+                    <div class="list">
+                      <ul>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
               </div>` ,
   props: ['dto', 'options'],
 });
