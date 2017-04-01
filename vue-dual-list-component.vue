32e415da4d0d650ac9a5a901a011c0472228cb18
mikemajesty <mikemajesty2011@gmail.com>
2017-03-31 22:04:57 -0300

    add vue component

---
+++ vue-dual-list-component.vue
@@ -0,0 +1,144 @@
+<template>
+	<md-layout md-gutter class='vue-dual-list'>
+		<md-layout md-flex-small="100" md-flex-medium="100" md-hide-xsmall>
+
+			<md-input-container v-if='options.selectOptions.isRequired' style='margin-bottom: 0px'>
+				<label for="filter">{{options.label}}</label>
+				<md-select name="filter" id="filter" v-model="search" required>
+					<md-option v-for='item in options.categories' :value='item.id'>
+						<b>{{ options.selectOptions.uppercase ? item.category.toUpperCase() : item.category}}</b>
+					</md-option>
+				</md-select>
+			</md-input-container>
+
+			<md-input-container v-if='!options.selectOptions.isRequired' style='margin-bottom: 0px'>
+				<label for="filter">{{options.label}}</label>
+				<md-select name="filter" id="filter" v-model="search">
+					<md-option value=''></md-option>
+					<md-option v-for='item in options.categories' :value='item.id'>
+						<b>{{ options.selectOptions.uppercase ? item.category.toUpperCase() : item.category}}</b>
+					</md-option>
+				</md-select>
+			</md-input-container>
+
+			<md-layout md-flex='50' md-align="center">
+				<md-button class="md-raised" v-on:click.native='transferToRight(options.items.indexOf(item))'>
+					{{options.buttonOption.textLeft}}
+					<md-icon style='padding-bottom: 4px;'>arrow_forward</md-icon>
+				</md-button>
+			</md-layout>
+
+			<md-layout md-flex='50' md-align="center">
+				<md-button class="md-raised" v-on:click.native='transferToLeft(-1)'>
+					<md-icon style='padding-bottom: 4px;'>arrow_back</md-icon>
+					{{ options.buttonOption.textRight }}
+				</md-button>
+			</md-layout>
+
+			<md-layout md-flex class='list' v-bind:class="options.resizeBox">
+				<ul class='pd'>
+					<li v-for='item in filtering'>
+						<a href='#' v-on:click='transferToRight(options.items.indexOf(item))' v-bind:style="{ color: options.colorItems || '#1E90FF' }">
+							<span v-if='!search'> 
+								{{ item.category }} -
+							</span> {{ item.name }}&nbsp;&rArr;</a>
+					</li>
+				</ul>
+			</md-layout>
+
+			<div style='padding-left: 10px' />
+
+			<md-layout md-flex class='list' v-bind:class="options.resizeBox">
+				<ul class='pd'>
+					<li v-for='item in options.selectedItems'>
+						<a href='#' v-on:click='transferToLeft(options.selectedItems.indexOf(item))' v-bind:style="{ color: options.colorItems || '#1E90FF'}">
+								&lArr;&nbsp;{{item.category}} - {{item.name}}
+						</a>
+					</li>
+				</ul>
+			</md-layout>
+		</md-layout>
+	</md-layout>
+</template>
+
+
+<style scoped>
+  .vue-dual-list .list {
+    border: 1px solid #999;
+    border-radius: 4px;
+    padding: 10px;
+    overflow-y: scroll;
+  }
+  
+  .vue-dual-list .xs {
+    height: 150px;
+  }
+  
+  .vue-dual-list .md {
+    height: 225px;
+  }
+  
+  .vue-dual-list .lg {
+    height: 350px;
+  }
+  
+  .vue-dual-list .xl {
+    height: 500px;
+  }
+  
+  .vue-dual-list ul.pd {
+    padding-left: 12px;
+  }
+</style>
+
+<script>
+	export default { 
+		props: {
+			'options': {
+				type: Object,
+				default: {},
+				required: true
+			}
+		},
+		data: function() {
+			return {
+				search: '',
+				item: {}
+			};
+		},
+		methods: {
+			transferToRight: function(index) {
+				if (index >= 0) {
+					this.options.selectedItems.push(this.options.items[index]);
+					this.options.items.splice(index, 1);
+				} else {
+					for (var cont = 0; cont < this.options.items.length; cont++) {
+						this.options.selectedItems.push(this.options.items[cont]);
+					}
+					this.options.items = [];
+				}
+			},
+			transferToLeft: function(index) {
+				if (index >= 0) {
+					this.options.items.push(this.options.selectedItems[index]);
+					this.options.selectedItems.splice(index, 1);
+				} else {
+					for (var cont = 0; cont < this.options.selectedItems.length; cont++) {
+						this.options.items.push(this.options.selectedItems[cont]);
+					}
+					this.options.selectedItems = [];
+				}
+			}
+		},
+		computed: {
+			filtering: function() {
+				if (this.search) {
+					return this.options.items.filter((item) => {
+						return item.category.toLowerCase() === this.search.toLowerCase();
+					});
+				}
+				return this.options.items;
+			}
+		}
+	}
+</script>
